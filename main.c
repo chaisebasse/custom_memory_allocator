ddcd5fdfb0dbf1b7ab689d7a0bb55f93a884b6f8
Ermanno di Giulio <digiulioermanno@gmail.com>
2024-05-31 22:37:46 +0200

    implemented coalescing logic

--- main.c
+++ main.c
@@ -21,9 +21,20 @@ void my_free(void *ptr);
 
 Block* free_list = NULL;
 
+void print_block_info() {
+  Block *block = free_list;
+  while (block) {
+    printf("size: %zu, free: %s, next: %p, prev: %p\n", block->size, block->free ? "true" : "false", block->next, block->prev);
+    block = block->next;
+  }
+  printf("\n");
+}
+
 int main() {
   int* arr = (int*) my_malloc(10 * sizeof(int));
-  if (arr == NULL) {
+  int* arr1 = (int*) my_malloc(5 * sizeof(int));
+  int* arr2 = (int*) my_malloc(8 * sizeof(int));
+  if (arr == NULL || arr1 == NULL || arr2 == NULL) {
     fprintf(stderr, "my_malloc failed\n");
     return 1;
   }
@@ -34,10 +45,54 @@ int main() {
   }
   printf("\n");
 
+
+  Block *block = free_list;
+  Block *block2 = free_list;
+  do
+  {
+    printf("size : %i, free : %s, next : %p, prev : %p\n", (int)block->size, block->free ? "true" : "false", block->next, block->prev);
+
+    block = block2;
+    block2 = block2->next;
+  } while (block2);
+
   my_free(arr);
+  my_free(arr1);
+  my_free(arr2);
   return 0;
 }
 
+Block *find_free_block(Block **last, size_t size) {
+  Block *current = free_list;
+  while (current && !(current->free && current->size >= size)) {
+    *last = current;
+    current = current->next;
+  }
+
+  return current;
+}
+
+Block *request_space(Block *last, size_t size) {
+  Block *block;
+  block = sbrk(0);
+
+  void *request = sbrk(size + BLOCK_SIZE);
+  if (request ==  (void *)-1) {
+    perror("sbrk fail (request_space)");
+    return NULL;
+  }
+
+  block->size = size;
+  block->next = NULL;
+  block->free = false;
+  block->prev = last;
+
+  if (last) {
+    last->next = block;
+  }
+
+  return block;
+}
 
 void *my_malloc(size_t size) {
   if (size <= 0) {
@@ -79,37 +134,28 @@ void my_free(void *ptr) {
   }
 
   Block *block = (Block *)ptr - 1;
+  Block *pre_block = (Block *)ptr - 1;
   block->free = true;
-}
 
-Block *find_free_block(Block **last, size_t size) {
-  Block *current = free_list;
-  while (current && !(current->free && current->size >= size)) {
-    *last = current;
-    current = current->next;
+
+  // coalescing logic
+  if (block->next && block->next->free) {
+    block->size += BLOCK_SIZE + block->next->size;
+    block->next = block->next->next;
+    if (block->next) {
+      block->next->prev = block;
+    }
   }
 
-  return current;
-}
-
-Block *request_space(Block *last, size_t size) {
-  Block *block;
-  block = sbrk(0);
-
-  void *request = sbrk(size + BLOCK_SIZE);
-  if (request ==  (void *)-1) {
-    perror("sbrk fail (request_space)");
-    return NULL;
+  if (pre_block->prev && pre_block->prev->free) {
+    pre_block->size += BLOCK_SIZE + pre_block->prev->size;
+    pre_block->prev = pre_block->prev->prev;
+    if (pre_block->prev) {
+      pre_block->prev->next = pre_block;
+    }
+  } else {
+    if (!free_list || block < free_list) {
+      free_list = block;
+    }
   }
-
-  if (last) {
-    last->next = block;
-  }
-
-  block->size = size;
-  block->next = NULL;
-  block->free = false;
-  block->prev = last;
-
-  return block;
 }
